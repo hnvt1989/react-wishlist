c90a67dd7b3586b432ca7204b6a5161ec90347bf
Huy Nguyen <huy.nguyen@act-on.net>
2018-12-23 13:29:53 -0800

    commented out cover_image field

--- go-server/models/wish.go
+++ go-server/models/wish.go
@@ -7,6 +7,5 @@ import "gopkg.in/mgo.v2/bson"
 type Wish struct {
 	ID          bson.ObjectId `bson:"_id" json:"id"`
 	Name        string        `bson:"name" json:"name"`
-	//CoverImage  string        `bson:"cover_image" json:"cover_image"`
 	Description string        `bson:"description" json:"description"`
 }
