f23c23ae166bb6d57875a3a3b8b1f7873d89f584
Huy Nguyen <huy.nguyen@act-on.net>
2018-12-26 20:44:48 -0800

    added WishItem model

--- go-server/models/wish.go
+++ go-server/models/wish.go
@@ -2,11 +2,20 @@ package models
 
 import "gopkg.in/mgo.v2/bson"
 
-// Represents a wish, we uses bson keyword to tell the mgo driver how to name
+// Wish Represents a wish, we uses bson keyword to tell the mgo driver how to name
 // the properties in mongodb document
 type Wish struct {
 	ID          bson.ObjectId `bson:"_id" json:"id"`
 	Name        string        `bson:"name" json:"name"`
 	Description string        `bson:"description" json:"description"`
-	UserId      string        `bson:"userId" json:"userId"`		
+	UserID      string        `bson:"userId" json:"userId"`
+	Items       []WishItem    `bson:"items" json:"items"`
+}
+
+// WishItem Represent a Wish item
+type WishItem struct {
+	ID   int    `bson:"id" json:"id"`
+	Name string `bson:"name" json:"name"`
+	Note string `bson:"note" json:"note"`
+	URL  string `bson:"url" json:"url"`
 }
